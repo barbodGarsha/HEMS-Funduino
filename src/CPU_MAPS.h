110958e545b5169ffdc8c3fcc9fe1b39abc95ec1
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-12 14:46:59 +0100

    Update CPU_MAPS.h
    added CPU maps for:
    A2B
    A3
    A4
    A5

---
+++ src/CPU_MAPS.h
@@ -0,0 +1,38 @@
+#ifndef CPU_MAPS_h 
+#define CPU_MAPS_h
+
+// A2B CPU MAP
+    #define A2B_LED1 3 //LED1 on pin 3
+    #define A2B_LED2 4 //LED2 on pin 4
+    #define A2B_LED3 5 //LED3 on pin 5
+    #define A2B_LED4 6 //LED4 on pin 6
+    #define A2B_LED5 7 //LED5 on pin 7
+
+// A3 CPU MAP
+    #define A3_LED1 3 //LED1 on pin 3
+
+// A4 CPU MAP
+    #define A4_LED1 3 //LED1 on pin 3
+    #define A4_PIEPS 2 //Pieps on pin 2
+
+// A4A CPU MAP
+    #define A4A_LED1 3 //LED1 on pin 3
+    #define A4A_LED2 4 //LED2 on pin 4
+    #define A4A_LED3 5 //LED3 on pin 5
+    #define A4A_PIEPS 2 //Pieps on pin 2
+
+// A4B CPU MAP
+    #define A4B_LED1 3 //LED1 on pin 3
+    #define A4B_LED2 5 //LED2 on pin 4
+    #define A4B_LED3 4 //LED3 on pin 5
+    #define A4B_PIEPS 2 //Pieps on pin 2
+
+// A5 CPU MAP
+    #define A5_LED_R 3 //LED Red on pin 3
+    #define A5_LED_G 5 //LED Green on pin 4
+    #define A5_LED_B 6 //LED Blue on pin 5
+
+
+
+
+#endif
