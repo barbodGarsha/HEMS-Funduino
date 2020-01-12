b7de3b61f585a5b526321a4b0ad0019c63fc673b
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-12 16:05:35 +0100

    update program.h/cpp
    added some functions

---
+++ src/programs.cpp
@@ -0,0 +1,33 @@
+#include "HEMS_Funduino.h"
+
+int A2B()
+{
+    program_A2B_init();
+    for (;;)
+    {
+        /* code */
+    }
+    
+}
+
+int A3()
+{
+    program_A3_init();
+    for (;;)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
+
+int A4()
+{
+    program_A4_init();
+    for (;;)
+    {
+        /* code */
+    }
+
+    return 0;
+}
---
+++ src/programs.h
@@ -0,0 +1,10 @@
+#ifndef programs_h 
+#define programs_h
+
+int A2B();
+
+int A3();
+
+int A4();
+
+#endif
