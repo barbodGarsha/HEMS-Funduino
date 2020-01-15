72ec7e1995e9bc202fe5794c59f7ddf3d23a334b
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-15 16:31:01 +0100

    Update basic_functions.cpp
    added functions to turn on and off the LEDs with fade effect

--- src/basic_functions.cpp
+++ src/basic_functions.cpp
@@ -14,3 +14,35 @@ void led_blink(int n, int ms)
   delay_ms(ms);               // wait
   digitalWrite(n, LOW);    // turn the LED off
 }
+
+// Turns on the LED with fade
+// At the end this function returns the brightness just in case we need it for example in led_fade_on_off it is needed
+int led_fade_on(int n, int brightness, int brightness_max, int fade_schritte, int delay_ms)
+{
+  for(brightness; brightness < brightness_max; brightness += fade_schritte)
+  {
+    analogWrite(n, brightness);
+    delay(delay_ms);
+  }
+  return brightness;
+}
+
+// Turns off the LED with fade
+// At the end this function returns the brightness just in case we need it for example in led_fade_on_off it is needed
+int led_fade_off(int n, int brightness, int brightness_min, int fade_schritte, int delay_ms)
+{
+  for(brightness; brightness > brightness_min; brightness -= fade_schritte)
+  {
+    analogWrite(n, brightness);
+    delay(delay_ms);
+  }
+  return brightness;
+}
+
+// Turns on and off the LED with fade
+void led_fade_on_off(int n, int delay_ms)
+{
+  int brightness = 0;
+  brightness = led_fade_on(n, brightness, BRIGHTNESS_MAX, FADE_STEPS, 100);
+  led_fade_off(n, brightness, BRIGHTNESS_MIN, FADE_STEPS, 100);
+}
--- src/basic_functions.h
+++ src/basic_functions.h
@@ -7,5 +7,13 @@ void delay_ms(uint16_t ms);
 // This functions makes the LED blink
 void led_blink(int n, int delay_ms);
 
+// Turns on the LED with fade
+int led_fade_on(int n, int helligkeit, int helligkeit_min, int helligkeit_max, int fade_schritte, int delay_ms);
+  
+// Turns off the LED with fade
+int led_fade_off(int n, int helligkeit, int helligkeit_min, int helligkeit_max, int fade_schritte, int delay_ms);
+
+// Turns on and off the LED with fade
+void led_fade_on_off(int n, int delay_ms);
 
 #endif
