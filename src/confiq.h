a6dd6ec68e2566b0b17a6c1595e467ddc4a637bf
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-15 15:48:07 +0100

    Update confiq.h
    added CLI
    added Start-up Mesaage

--- src/confiq.h
+++ src/confiq.h
@@ -1,6 +1,16 @@
 #ifndef confiq_h 
 #define confiq_h
 
+//CLI enable
+#define CLI_ENABLE
+
+//Start-Up message enable
+#define STRAT_UP_MESSAGE_ENABLE
+#ifdef STRAT_UP_MESSAGE_ENABLE
+    #define STRAT_UP_MESSAGE "Developed by Barbod Garshasbi\n"
+#endif
+
+
 // Serial baud rate
 #define BAUD_RATE 9600
 
