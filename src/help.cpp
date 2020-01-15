fa6c2254f522be86d20ecd63e62197bf54e5aea3
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-15 17:30:58 +0100

    added help.h/cpp

---
+++ src/help.cpp
@@ -0,0 +1,63 @@
+#include "HEMS_Funduino.h"
+
+int help_line(String help_line)
+{
+    if (help_line == HPA2B)
+    {
+        #ifdef CLI_ENABLE
+        
+        printPgmString(PSTR("\n"));
+        printPgmString(PSTR("the CPU map for PA2B : \n"));
+
+        printPgmString(PSTR("LED1 is on digital pin : "));
+        printInteger(A2B_LED1);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("LED2 is on digital pin : "));
+        printInteger(A2B_LED2);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("LED3 is on digital pin : "));
+        printInteger(A2B_LED3);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("LED4 is on digital pin : "));
+        printInteger(A2B_LED4);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("LED5 is on digital pin : "));
+        printInteger(A2B_LED5);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("\n"));
+    
+        #endif
+    }
+    else if (help_line == HPA3)
+    {
+        #ifdef CLI_ENABLE
+        
+        printPgmString(PSTR("\n"));
+        printPgmString(PSTR("the CPU map for PA3 : \n"));
+
+        printPgmString(PSTR("LED1 is on digital pin : "));
+        printInteger(A3_LED1);
+        printPgmString(PSTR("\n"));
+        
+        printPgmString(PSTR("\n"));
+        
+        #endif
+    
+    }
+    else if (help_line == HPA4)
+    {
+        
+    }
+    else
+    {
+        return 1;
+    }
+    
+    
+           
+}
---
+++ src/help.h
@@ -0,0 +1,9 @@
+#ifndef help_h 
+
+#define HPA2B "HPA2B"
+#define HPA3 "HPA3"
+#define HPA4 "HPA4"
+
+int help_line(String help_line);
+
+#endif
