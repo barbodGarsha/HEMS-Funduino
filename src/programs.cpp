3f6d0a08672b0d3170b9781fbbf4ed9cc15575ea
barbodGarsha <barbodgarshasbi79@gmail.com>
2020-01-15 16:32:48 +0100

    Update programs.cpp
    forget ro commit this last time :|

--- src/programs.cpp
+++ src/programs.cpp
@@ -7,9 +7,26 @@ volatile uint8_t realtime_command = 0;
 //TO DO2: realtime command... it just works for reset command
 void set_realtime_command(){realtime_command = 1;}
 
+//check if there is an interrupt
+int check_realtime()
+{
+    if (realtime_command)
+    {
+        //TO DO2: realtime command... it just works for reset command 
+
+        #ifdef CLI_ENABLE
+        printPgmString(PSTR("Exiting...\n"));
+        #endif 
+        //Reset
+        return 1;
+    }
+    return 0;
+    
+}
+
 // execute the program based on the program code we got from the PC/Laptop
 // for example PA2B means the Program for the Aufgabe 2B
-void program_change(String program)
+int program_change(String program)
 {
     
     if (program == PA2B)
@@ -21,6 +38,9 @@ void program_change(String program)
     }
     else if(program == PA3)
     {
+        #ifdef CLI_ENABLE
+        printPgmString(PSTR("PA3 is running\n"));
+        #endif
         program_A3_init();
     }
     else if(program == PA4)
@@ -29,8 +49,10 @@ void program_change(String program)
     }
     else
     {
-        /* TO DO1: no program with this code error */
+        return 1;
     }
+    
+    return 0;
     
     
     
@@ -43,23 +65,19 @@ int program_A2B()
     program_A2B_init();
     for (;;)
     {        
+        // make the LEDs blink
         led_blink(A2B_LED1, PA2B_BLINK_DELAY_MS);
         led_blink(A2B_LED2, PA2B_BLINK_DELAY_MS);
         led_blink(A2B_LED3, PA2B_BLINK_DELAY_MS);
         led_blink(A2B_LED4, PA2B_BLINK_DELAY_MS);
         led_blink(A2B_LED5, PA2B_BLINK_DELAY_MS);
-        delay_ms(PA2B_LOOP_DELAY_MS);
-        if (realtime_command)
-        {
-            //TO DO2: realtime command... it just works for reset command 
 
-            #ifdef CLI_ENABLE
-            printPgmString(PSTR("Exiting...\n"));
-            #endif 
-            //Reset
-            break;
-        }
-        
+        // wait
+        delay_ms(PA2B_LOOP_DELAY_MS);
+
+        //TO DO2: realtime command... it just works for reset command 
+        // check if there is an interrupt 
+        if (check_realtime()) { break; }
     }
     
     return 0;
@@ -70,7 +88,15 @@ int program_A3()
     program_A3_init();
     for (;;)
     {
-        /* code */
+        // trun on and off the LED with fade
+        led_fade_on_off(A3_LED1, 10);
+
+        // wait
+        delay(PA3_LOOP_DELAY_MS);
+
+        //TO DO2: realtime command... it just works for reset command 
+        // check if there is an interrupt 
+        if (check_realtime()) { break; }
     }
     
     return 0;
